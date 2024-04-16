d4dd8a9907f2bff6b8343b61caa93cc854b50c5d
Sai-prudhvi1483 <63552508+Sai-prudhvi1483@users.noreply.github.com>
2024-04-16 09:07:31 +0530

    Update script.py

--- script.py
+++ script.py
@@ -10,27 +10,21 @@ REPO_OWNER = "Sai-prudhvi1483"
 REPO_NAME = "prudhvi"
 
 
-# Environment variable to update
-ENV_VAR_NAME = "VERSION"
+# Secret to update
+SECRET_NAME = "VERSION"
 NEW_VALUE = "1.0.9"  # New version number
 
-def update_env_var():
+def update_secret():
     try:
         g = Github(GITHUB_TOKEN)
         repo = g.get_repo(f"{REPO_OWNER}/{REPO_NAME}")
 
-        # Get the environment named 'production'
-        env = repo.get_environment("production")
+        # Update the secret
+        repo.create_or_update_secret(SECRET_NAME, NEW_VALUE)
 
-        # Delete the old environment variable
-        env.remove_secret(ENV_VAR_NAME)
-
-        # Create a new environment variable with the updated value
-        env.add_secret(ENV_VAR_NAME, NEW_VALUE)
-
-        print(f"Environment variable {ENV_VAR_NAME} updated successfully!")
+        print(f"Secret {SECRET_NAME} updated successfully!")
     except Exception as e:
-        print(f"Error updating environment variable: {str(e)}")
+        print(f"Error updating secret: {str(e)}")
 
 if __name__ == "__main__":
-    update_env_var()
+    update_secret()
